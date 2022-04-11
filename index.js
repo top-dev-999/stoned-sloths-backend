8b2d652f7bec67666733e787228c37bcd6c4d860
top-dev-999 <topdeveloper1999@outlook.com>
2022-04-11 18:00:12 +0900

    update cors

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
+const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5005;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Method", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  next();
+});
 
 app.post("/api/email", (req, res, next) => {
   //if get email from client side
